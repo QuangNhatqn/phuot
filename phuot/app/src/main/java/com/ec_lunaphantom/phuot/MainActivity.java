f17102f99f206081655c91d04f2bcaac30dd0702
Võ Quang Nhật <vnhatqn@gmail.com>
2019-11-04 15:05:54 +0700

    (Nhật) First commit project phuot

---
+++ phuot/app/src/main/java/com/ec_lunaphantom/phuot/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ec_lunaphantom.phuot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
